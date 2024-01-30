3475641cde666687e426654977f3cc44e9e1eb58
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2024-01-30 12:52:16 -0500

    fixed url in readme

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+# index.py
 import requests
 from urllib import parse
 from http.server import BaseHTTPRequestHandler
@@ -10,35 +11,28 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        try:
-            if 'country' in dic:
-                response = requests.get(f'https://restcountries.com/v3.1/name/{dic["country"]}?fields=name,capital')
-                response.raise_for_status()  # Raise HTTPError for bad responses
-                country_list = response.json()
-                country_obj = country_list[0]
-                capital = country_obj['capital'][0]
-                message = f'The capital of {country_obj["name"]["common"]} is {capital}'
-            elif 'capital' in dic:
-                response = requests.get(f'https://restcountries.com/v3.1/capital/{dic["capital"]}?fields=name,capital')
-                response.raise_for_status()
-                capital_list = response.json()
-                capital_obj = capital_list[0]
-                country = capital_obj['name']['common']
-                message = f'{dic["capital"]} is the capital of {country}'
-            else:
-                message = 'Invalid query parameters'
-                response = {
-                    'statusCode': 400,
-                    'body': 'Invalid query parameters'
-                }
-        except requests.exceptions.RequestException as e:
-            message = 'Error making API request'
+        # Choose the appropriate handler based on the presence of 'country' or 'capital'
+        if 'country' in dic:
+            response = requests.get(f'https://restcountries.com/v3.1/name/{dic["country"]}?fields=name,capital')
+            country_list = response.json()
+            country_obj = country_list[0]
+            capital = country_obj['capital'][0]
+            message = f'The capital of {country_obj["name"]["common"]} is {capital}'
+        elif 'capital' in dic:
+            response = requests.get(f'https://restcountries.com/v3.1/capital/{dic["capital"]}?fields=name,capital')
+            capital_list = response.json()
+            capital_obj = capital_list[0]
+            country = capital_obj['name']['common']
+            message = f'{dic["capital"]} is the capital of {country}'
+
+        else:
+            message = 'Invalid query parameters'
             response = {
-                'statusCode': 500,
-                'body': f'Internal Server Error: {str(e)}'
+                'statusCode': 400,
+                'body': 'Invalid query parameters'
             }
         
-        self.send_response(response['statusCode'])
+        self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(message.encode('utf-8'))
